e619a0a6fcf0883ec223ffb0b11609ae7cad327c
makhijagaurav <makhijagaurav98@gmail.com>
2020-02-04 17:27:45 +0530

    Added cleaning migration

---
+++ database/migrations/2020_02_04_112321_create_cleanings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCleaningsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cleanings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('cleaner_name');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cleanings');
+    }
+}
