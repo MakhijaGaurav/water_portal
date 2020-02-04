4a4731e690856d7aa2d9a14cf0535afa22d5b601
makhijagaurav <makhijagaurav98@gmail.com>
2020-02-04 17:32:07 +0530

    Added families migration

---
+++ database/migrations/2020_02_04_111520_create_families_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFamiliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('families', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('surname',255);
+            $table->bigInteger('head');
+            $table->string('address_first_line',255);
+            $table->string('address_second_line',255);
+            $table->string('address_landmark',255);
+            $table->string('address_city',255);
+            $table->string('address_state',255);
+            $table->string('address_zip',255);
+            $table->timestamps();
+            $table->softDeletesg();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('families');
+    }
+}
