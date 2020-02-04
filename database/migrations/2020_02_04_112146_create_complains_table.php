8e278415670663f0a566cbc0fb79415953a47eeb
makhijagaurav <makhijagaurav98@gmail.com>
2020-02-04 17:29:22 +0530

    Created complains migration

---
+++ database/migrations/2020_02_04_112146_create_complains_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComplainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complains', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('complaint_name');
+            $table->string('msg',255);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complains');
+    }
+}
