d3ff149d385fda6d8c4107b9db445d81b36f9cd7
mokshManchandani <moksh.manchandani@gmail.com>
2020-02-05 15:45:39 +0530

    Added fillable, softDelete and Timestamps

--- app/Family.php
+++ app/Family.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Family extends Model
 {
-    //
+    protected $fillable = ["surname","head","address_first_line","address_second_line","address_landmark","address_city","address_state","address_zip"];
+    public $timestamps = true;
+    use SoftDeletes;
 }
