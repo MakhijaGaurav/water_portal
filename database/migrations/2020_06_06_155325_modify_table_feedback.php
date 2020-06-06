8221ea9e21fb2f9c3c259da01c43e28dcfe4e0e1
makhijagaurav <makhijagaurav98@gmail.com>
2020-06-06 16:06:16 +0530

    modified schema: feedback

--- database/migrations/2020_06_06_155325_modify_table_feedback.php
+++ database/migrations/2020_06_06_155325_modify_table_feedback.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTableFeedback extends Migration
+class ModifyTableFeedback extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class AlterTableFeedback extends Migration
     public function up()
     {
         Schema::table('feedback', function (Blueprint $table) {
-            //
+            $table->string('user_id')->change();
+            $table->rename('user_id','name');
         });
     }
 
@@ -25,7 +26,7 @@ class AlterTableFeedback extends Migration
      */
     public function down()
     {
-        Schema::table('feedback', function (Blueprint $table) {
+        Schema::table(''feedback'', function (Blueprint $table) {
             //
         });
     }
